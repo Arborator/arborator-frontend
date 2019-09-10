326fe5c5c226b757eeede01cbbba801b1896c587
gguibon <gael.guibon@inria.fr>
2019-09-10 12:12:11 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# arborator-frontend
-VueJs based user interface for Arborator-flask
+# arborator (arborator-frontend)
+
+Frontend for arborator draft
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/layouts/TempLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          Arborator
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'TempLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>

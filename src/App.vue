c94a8e8dc7303c2546bc830dcf5c9172422681dd
gguibon <gael.guibon@inria.fr>
2019-09-30 11:47:54 +0200

    better ui dialog grew matcher with templates

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,32 @@
 </template>
 
 <script>
-
+import VueCookies from 'vue-cookies';
+import Store from './store/index';
+import api from './boot/backend-api';
+VueCookies.config('7d');
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      store: Store
+    }
+  },
+  mounted() {
+    var token = VueCookies.get('authomatic');
+    console.log(token);
+  },
+  methods: {
+    parseJwt: function (token) {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      return JSON.parse(jsonPayload);
+    }
+  }
 }
 </script>
 

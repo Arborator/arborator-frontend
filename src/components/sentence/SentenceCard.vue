a5b3c85e97cdd87b0d47b464d61536f7cad4339b
kirianguiller <kirianguiller@gmail.com>
2020-10-13 19:24:51 +0200

    <add> save as Emmett

--- src/components/sentence/SentenceCard.vue
+++ src/components/sentence/SentenceCard.vue
@@ -47,8 +47,8 @@
           @click="save('teacher')"
         >
           <q-tooltip>Save as teacher</q-tooltip>
-        </q-btn>        
-        
+        </q-btn>
+
         <q-btn
           v-if="isBernardCaron"
           flat
@@ -168,7 +168,13 @@
         />
       </q-tabs>
       <q-separator />
-      <q-tab-panels v-model="tab" keep-alive>
+      <q-tab-panels
+        v-model="tab"
+        keep-alive
+        @transition="transitioned"
+        :animated="animated ? true : false"
+        :class="animated ? 'easeOutSine' : ''"
+      >
         <q-tab-panel
           v-for="(tree, user) in filteredConlls"
           :key="user"
@@ -179,16 +185,20 @@
             <q-card-section
               :class="($q.dark.isActive ? '' : '') + ' scrollable'"
             >
+              <!-- :reactiveSentence="reactiveSentence" -->
               <VueDepTree
+                v-if="reactiveSentencesObj"
                 :conll="tree"
+                :reactiveSentence="reactiveSentencesObj[user]"
+                :teacherReactiveSentence="reactiveSentencesObj['teacher']"
                 :sentenceId="sentenceId"
                 :sentenceBus="sentenceBus"
                 :userId="user"
                 :conllSavedCounter="conllSavedCounter"
                 :teacherConll="
                   (exerciseLevel <= 2 || isAdmin) && user !== 'teacher'
-                    ? sentenceData.conlls.teacher
-                    : ''
+                    ? reactiveSentencesObj[user].teacher
+                    : {}
                 "
               ></VueDepTree>
             </q-card-section>
@@ -208,7 +218,10 @@
     <MetaDialog :sentenceBus="sentenceBus" />
     <ConlluDialog :sentenceBus="sentenceBus" />
     <ExportSVG :sentenceBus="sentenceBus" />
-    <TokenDialog :sentenceBus="sentenceBus" @changed:metaText="changeMetaText" />
+    <TokenDialog
+      :sentenceBus="sentenceBus"
+      @changed:metaText="changeMetaText"
+    />
     <StatisticsDialog
       :sentenceBus="sentenceBus"
       :conlls="sentenceData.conlls"
@@ -236,9 +249,6 @@ import ExportSVG from "./ExportSVG.vue";
 import TokenDialog from "./TokenDialog.vue";
 import StatisticsDialog from "./StatisticsDialog.vue";
 
-// ArboratorDraft is the svg tree handler
-const arboratorDraft = new ArboratorDraft();
-
 export default {
   name: "SentenceCard",
   components: {
@@ -256,7 +266,9 @@ export default {
   data() {
     return {
       sentenceBus: new Vue(), // Event/Object Bus that communicate between all components
+      reactiveSentencesObj: {},
       tab: "",
+      animated: false,
       sentenceData: this.$props.sentence,
       EMMETT: "emmett.strickland",
       graphInfo: { conllGraph: null, dirty: false, redo: false, user: "" },
@@ -316,15 +328,20 @@ export default {
         return this.sentenceData.conlls;
       }
     },
-    isBernardCaron() {
+        isBernardCaron() {
       return ((this.$store.getters["user/getUserInfos"].username == "bernard.l.caron") || (this.$store.getters["user/getUserInfos"].username == "kirianguiller"))
     }
   },
-  mounted() {
+  created() {
     this.shownmetanames = this.$store.getters[
       "config/getProjectConfig"
     ].shownmeta;
 
+    for (const [userId, conll] of Object.entries(this.sentence.conlls)) {
+      const reactiveSentence = new ReactiveSentence();
+      reactiveSentence.fromConll(conll);
+      this.reactiveSentencesObj[userId] = reactiveSentence;
+    }
   },
   methods: {
     // to delete KK
@@ -401,38 +418,36 @@ export default {
      * @returns void
      */
     save(mode) {
-      const currentTreeUser = this.tab;
-      var conll = this.sentenceBus[currentTreeUser].exportConll();
+      const openedTreeUser = this.tab;
+      // var conll = this.sentenceBus[currentTreeUser].exportConll();
 
       var changedConllUser = this.$store.getters["user/getUserInfos"].username;
       if (mode == "teacher") {
         changedConllUser = "teacher";
-      }
+      }      
       if (mode == this.EMMETT) {
         changedConllUser = this.EMMETT;
       }
+      const metaToReplace = {
+        user_id: changedConllUser,
+        timestamp: Math.round(Date.now()),
+      };
 
-      // TODO add this METAedit() in the tree object
-      conll = conll.replace(
-        /# user_id = .+\n/,
-        "# user_id = " + changedConllUser + "\n"
-      );
+      const exportedConll = this.reactiveSentencesObj[
+        openedTreeUser
+      ].exportConllWithModifiedMeta(metaToReplace);
 
-      const timestamp = Math.round(Date.now());
-      conll = conll.replace(
-        /# timestamp = \d+(\.\d*)?\n/,
-        "# timestamp = " + timestamp + "\n"
-      );
       var data = {
         trees: [
           {
             sent_id: this.sentenceId,
-            conll: conll,
+            conll: exportedConll,
             sample_name: this.$props.sentence.samplename,
           },
         ],
         user_id: changedConllUser,
       };
+      // console.log("data", data);
       api
         .saveTrees(this.$route.params.projectname, data)
         .then((response) => {
@@ -459,7 +474,14 @@ export default {
         .catch((error) => {
           this.$store.dispatch("notifyError", { error: error });
         });
-      // var conll = this.sentenceBus[this.tab]
+    },
+    transitioned() {
+      if (this.exportedConll) {
+        this.reactiveSentencesObj[this.changedConllUser].fromConll(
+          this.exportedConll
+        );
+        this.exportedConll = "";
+      }
     },
     /**
      * Set the graph infos according to the event payload. This event shoudl be trigerred from the ConllGraph
@@ -485,8 +507,9 @@ export default {
      * @returns void
      */
     changeMetaText(newMetaText) {
-      this.sentenceData.sentence = newMetaText
+      this.sentenceData.sentence = newMetaText;
     },
+
     showNotif(position, alert) {
       const {
         color,
@@ -514,8 +537,98 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 .scrollable {
   overflow: scroll;
 }
+
+.custom-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.custom-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.custom-fade-enter, .custom-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+/* 
+.easeInOutQuart .q-transition--slide-right-enter-active,
+.easeInOutQuart .q-transition--slide-left-enter-active,
+.easeInOutQuart .q-transition--slide-up-enter-active,
+.easeInOutQuart .q-transition--slide-down-enter-active,
+.easeInOutQuart .q-transition--slide-right-leave-active,
+.easeInOutQuart .q-transition--slide-left-leave-active,
+.easeInOutQuart .q-transition--slide-up-leave-active,
+.easeInOutQuart .q-transition--slide-down-leave-active {
+  transition: transform 0.3s cubic-bezier(0.77, 0, 0.175, 1) !important;
+} */
+
+/* .easeOutQuad .q-transition--slide-right-enter-active,
+.easeOutQuad .q-transition--slide-left-enter-active,
+.easeOutQuad .q-transition--slide-up-enter-active,
+.easeOutQuad .q-transition--slide-down-enter-active,
+.easeOutQuad .q-transition--slide-right-leave-active,
+.easeOutQuad .q-transition--slide-left-leave-active,
+.easeOutQuad .q-transition--slide-up-leave-active,
+.easeOutQuad .q-transition--slide-down-leave-active {
+  transition: transform 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) !important;
+} */
+/* 
+.easeOutSine .q-transition--slide-right-enter-active,
+.easeOutSine .q-transition--slide-left-enter-active {
+  transition: opacity 0.5s !important;
+}
+
+.easeOutSine .q-transition--slide-right-leave-active,
+.easeOutSine .q-transition--slide-left-leave-active {
+  transition: transform 3s cubic-bezier(0.39, 0.575, 0.765, 1) !important;
+}
+
+.easeOutSine .q-transition--slide-right-enter-active,
+.easeOutSine .q-transition--slide-left-enter-active {
+  transition: opacity 0.1s !important;
+  transition-delay: 2s !important;
+
+} */
+
+.easeOutSine.q-transition--slide-right-leave-active,
+.easeOutSine.q-transition--slide-left-leave-active {
+  transition: opacity 1s !important;
+}
+
+.easeOutSine.q-transition--slide-right-enter-active,
+.easeOutSine.q-transition--slide-left-enter-active {
+  transition: opacity 1s !important;
+}
+/* transition-delay: 2s !important; */
+
+.easeOutSine.q-transition--slide-right-enter,
+.easeOutSine.q-transition--slide-left-enter {
+  opacity: 0 !important;
+  transition-delay: 2s !important;
+}
+
+.easeOutSine.q-transition--slide-right-leave-to,
+.easeOutSine.q-transition--slide-left-leave-to {
+  opacity: 0 !important;
+}
+
+.easeOutSine .q-transition--slide-right-leave-from,
+.easeOutSine .q-transition--slide-left-leave-from {
+  opacity: 1 !important;
+}
+
+/* .easeOutCubic .q-transition--slide-right-enter-active,
+.easeOutCubic .q-transition--slide-left-enter-active,
+.easeOutCubic .q-transition--slide-up-enter-active,
+.easeOutCubic .q-transition--slide-down-enter-active,
+.easeOutCubic .q-transition--slide-right-leave-active,
+.easeOutCubic .q-transition--slide-left-leave-active,
+.easeOutCubic .q-transition--slide-up-leave-active,
+.easeOutCubic .q-transition--slide-down-leave-active { */
+/* easeOutCubic */
+/* transition: transform 0.3s cubic-bezier(0.215, 0.61, 0.355, 1) !important;
+} */
 </style>
--- src/components/sentence/VueDepTree.vue
+++ src/components/sentence/VueDepTree.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="sentencebox">
-    <svg :id="svgID" />
+  <div>
+    <q-btn flat round dense icon="refresh" @click="dummyfct"
+      ><q-tooltip>See your annotation errors</q-tooltip>
+    </q-btn>
+    <div class="sentencebox">
+      <svg :id="svgID" />
+    </div>
   </div>
 </template>
 
@@ -16,6 +21,8 @@ export default {
     "userId",
     "conllSavedCounter",
     "teacherConll",
+    "reactiveSentence",
+    "teacherReactiveSentence",
   ],
   data() {
     return {
@@ -26,36 +33,22 @@ export default {
   },
   computed: {
     svgID() {
-      return `svg-${this.sentenceId}-${this.userId}`.replaceAll(".", "-");
+      return `svg-${this.sentenceId}-${this.userId}`.replaceAll(".","-");
     },
-    shownfeatures() {
+    shownFeatures() {
       return this.$store.getters["config/shownfeatures"];
-      // return ["FORM", "UPOS", "LEMMA", "FEATS.Aspect", "MISC.Gloss"];
-    },
-  },
-  watch: {
-    conllSavedCounter: function (val) {
-      console.log("KK watcher", this.userId)
-      this.sentenceSVG.treeJson = conllToJson(this.conll).treeJson;
-      this.sentenceSVG.metaJson = conllToJson(this.conll).metaJson;
-      this.sentenceSVG.drawTree();
     },
   },
   mounted() {
-    console.log("KK this.conll", this.conll)
     const sentenceJson = conllToJson(this.conll);
-    console.log("KK step 0 ", sentenceJson)
 
-    const teacherTreeJson = (this.teacherConll && this.userId !== "teacher") ? conllToJson(this.teacherConll).treeJson : "";
-    this.sentenceSVG = new SentenceSVG(
-      "#" + this.svgID,
-      sentenceJson,
-      this.usermatches,
-      this.shownfeatures,
-      teacherTreeJson
-    );
-
-    // this.sentenceSVG.showDiffs(teacherTreeJson);
+    this.sentenceSVG = new SentenceSVG({
+      svgID: this.svgID,
+      reactiveSentence: this.reactiveSentence,
+      usermatches: this.usermatches,
+      shownFeatures: this.shownFeatures,
+      teacherReactiveSentence: this.teacherReactiveSentence,
+    });
 
     this.sentenceBus[this.userId] = this.sentenceSVG;
 
@@ -69,20 +62,15 @@ export default {
 
     this.sentenceBus.$on("tree-update:token", ({ token, userId }) => {
       if (userId == this.userId) {
-        this.sentenceSVG.updateToken(token);
-        this.sentenceSVG.refresh();
+        this.reactiveSentence.updateToken(token);
       }
     });
-
-    // this.sentenceBus.$on("saved:tree", () => {
-    // console.log(this.conll)
-    // this.sentenceSVG.treeJson = conllToJson(this.conll).tree;
-    // this.sentenceSVG.META = conllToJson(this.conll).META;
-    // this.sentenceSVG.drawTree();
-    // this.sentenceSVG.refresh();
-    // })
   },
   methods: {
+    dummyfct() {
+      console.log("KK", this.sentenceSVG);
+      this.sentenceSVG.refresh();
+    },
     svgClickHandler(e) {
       const clickedId = e.detail.clicked;
       const clickedToken = this.sentenceSVG.treeJson[clickedId];

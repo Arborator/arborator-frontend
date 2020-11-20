f0e6c1722dac4bc087c9eeb70c39335187153b48
kirianguiller <kirianguiller@gmail.com>
2020-11-20 10:32:17 +0100

    <change> hide Grew search and GetRelationTables sticky button for student in exercise mode

--- src/pages/Sample.vue
+++ src/pages/Sample.vue
@@ -36,98 +36,119 @@
       </div>
     </div>
 
-    <q-page-sticky
-      :position="breakpoint ? 'bottom-right' : 'bottom-right'"
-      :offset="breakpoint ? [18, 18] : [30, 80]"
-      style="z-index: 999"
+    <template
+      v-if="
+        !(
+          $store.getters['config/exerciseMode'] &&
+          !$store.getters['config/isTeacher']
+        )
+      "
     >
-      <q-btn
-        size="20px"
-        round
-        @click="searchDialog = !searchDialog"
-        color="primary"
-        icon="img:../statics/svg/g.svg"
+      <q-page-sticky
+        :position="breakpoint ? 'bottom-right' : 'bottom-right'"
+        :offset="breakpoint ? [18, 18] : [30, 80]"
+        style="z-index: 999"
       >
-        <q-tooltip content-class="bg-primary" content-style="font-size: 16px">
-          Search with Grew in this sample
-        </q-tooltip>
-      </q-btn>
-    </q-page-sticky>
-
-    <q-page-sticky
-      :position="breakpoint ? 'bottom-right' : 'bottom-right'"
-      :offset="breakpoint ? [18, 88] : [30, 10]"
-      style="z-index: 999"
-    >
-      <q-btn-group push flat rounded v-if="reltablebuttons">
-        <q-btn @click="getRelationTable('user')" push color="primary" no-caps>
-          <q-tooltip content-class="bg-primary" content-style="font-size: 16px">
-            View only my trees
-          </q-tooltip>
-          <q-avatar v-if="isLoggedIn" size="1.2rem"
-            ><img :src="avatar"
-          /></q-avatar>
-          <q-icon v-else name="account_circle" />
-        </q-btn>
         <q-btn
-          @click="getRelationTable('user_recent')"
-          push
+          size="20px"
+          round
+          @click="searchDialog = !searchDialog"
           color="primary"
-          no-caps
+          icon="img:../statics/svg/g.svg"
         >
           <q-tooltip content-class="bg-primary" content-style="font-size: 16px">
-            View my trees, filled up with the most recent trees
-          </q-tooltip>
-          <q-avatar v-if="isLoggedIn" size="1.2rem"
-            ><img :src="avatar"
-          /></q-avatar>
-          <q-icon v-else name="account_circle" />
-          <div>+</div>
-        </q-btn>
-        <q-btn
-          @click="getRelationTable('recent')"
-          push
-          icon="schedule"
-          color="primary"
-          no-caps
-          v-if="isAdmin || isSuperAdmin"
-        >
-          <q-tooltip content-class="bg-primary" content-style="font-size: 16px">
-            View most recent trees
+            Search with Grew in this sample
           </q-tooltip>
         </q-btn>
-        <q-btn
-          @click="getRelationTable('all')"
-          push
-          icon="ion-md-globe"
-          color="primary"
-          no-caps
-          v-if="isAdmin || isSuperAdmin"
-        >
-          <q-tooltip content-class="bg-primary" content-style="font-size: 16px">
-            View all trees
-          </q-tooltip>
-        </q-btn>
-      </q-btn-group>
-      <q-btn
-        size="20px"
-        round
-        @click="reltablebuttons = !reltablebuttons"
-        color="primary text-green"
-        icon="ion-md-grid"
+      </q-page-sticky>
+      <q-page-sticky
+        :position="breakpoint ? 'bottom-right' : 'bottom-right'"
+        :offset="breakpoint ? [18, 88] : [30, 10]"
+        style="z-index: 999"
       >
-        <q-tooltip content-class="bg-primary" content-style="font-size: 16px">
-          Get Relation Tables
-        </q-tooltip>
-      </q-btn>
-    </q-page-sticky>
+        <q-btn-group push flat rounded v-if="reltablebuttons">
+          <q-btn @click="getRelationTable('user')" push color="primary" no-caps>
+            <q-tooltip
+              content-class="bg-primary"
+              content-style="font-size: 16px"
+            >
+              View only my trees
+            </q-tooltip>
+            <q-avatar v-if="isLoggedIn" size="1.2rem"
+              ><img :src="avatar"
+            /></q-avatar>
+            <q-icon v-else name="account_circle" />
+          </q-btn>
+          <q-btn
+            @click="getRelationTable('user_recent')"
+            push
+            color="primary"
+            no-caps
+          >
+            <q-tooltip
+              content-class="bg-primary"
+              content-style="font-size: 16px"
+            >
+              View my trees, filled up with the most recent trees
+            </q-tooltip>
+            <q-avatar v-if="isLoggedIn" size="1.2rem"
+              ><img :src="avatar"
+            /></q-avatar>
+            <q-icon v-else name="account_circle" />
+            <div>+</div>
+          </q-btn>
+          <q-btn
+            @click="getRelationTable('recent')"
+            push
+            icon="schedule"
+            color="primary"
+            no-caps
+            v-if="isAdmin || isSuperAdmin"
+          >
+            <q-tooltip
+              content-class="bg-primary"
+              content-style="font-size: 16px"
+            >
+              View most recent trees
+            </q-tooltip>
+          </q-btn>
+          <q-btn
+            @click="getRelationTable('all')"
+            push
+            icon="ion-md-globe"
+            color="primary"
+            no-caps
+            v-if="isAdmin || isSuperAdmin"
+          >
+            <q-tooltip
+              content-class="bg-primary"
+              content-style="font-size: 16px"
+            >
+              View all trees
+            </q-tooltip>
+          </q-btn>
+        </q-btn-group>
+        <q-btn
+          size="20px"
+          round
+          @click="reltablebuttons = !reltablebuttons"
+          color="primary text-green"
+          icon="ion-md-grid"
+        >
+          <q-tooltip content-class="bg-primary" content-style="font-size: 16px">
+            Get Relation Tables
+          </q-tooltip>
+        </q-btn>
+      </q-page-sticky>
 
-    <q-dialog v-model="searchDialog" seamless position="right" full-width>
-      <GrewRequestCard
-        :parentOnSearch="onSearch"
-        :grewquery="$route.query.q || ''"
-      ></GrewRequestCard>
-    </q-dialog>
+      <q-dialog v-model="searchDialog" seamless position="right" full-width>
+        <!-- v-if="false" -->
+        <GrewRequestCard
+          :parentOnSearch="onSearch"
+          :grewquery="$route.query.q || ''"
+        ></GrewRequestCard>
+      </q-dialog>
+    </template>
 
     <q-dialog
       v-model="resultSearchDialog"
@@ -258,7 +279,7 @@ export default {
         .getSampleTrees(this.projectname, this.samplename)
         .then((response) => {
           this.sentences = response.data.sample_trees;
-          this.exerciseLevel = response.data.exercise_level
+          this.exerciseLevel = response.data.exercise_level;
           this.freezesentences();
           this.$forceUpdate();
           this.loading = false;

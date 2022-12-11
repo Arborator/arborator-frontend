66689e5434d860b4c165de93f5d14a4cc7a10ee4
unknown <hk_daoudi@esi.dz>
2022-12-11 23:51:34 +0100

    add: click on the frequency number to see the usage of the lexicon in the samples

--- src/components/lexicon/LexiconTableBase.vue
+++ src/components/lexicon/LexiconTableBase.vue
@@ -1,150 +1,169 @@
 <template>
-  <q-table
-    v-bind="$attrs"
-    ref="table"
-    v-model:selected="table.selected"
-    v-model:pagination="table.pagination"
-    selection="multiple"
-    :rows="getLexiconData"
-    row-key="key"
-    binary-state-sort
-    :rows-per-page-options="[50]"
-    :columns="table.fields"
-    :visible-columns="table.visibleColumns"
-    card-class="shadow-8"
-    table-style="max-height:80vh"
-    :loading="lexiconLoading"
-    loading-label="loading"
-    :filter="table.filter"
-    :class="($q.dark.isActive ? 'my-sticky-header-table-dark' : 'my-sticky-header-table') + ' rounded-borders'"
-    @row-click="onRowClick"
-  >
-    <template #header-selection="scope">
-      <q-checkbox v-model="scope.selected" />
-    </template>
+  <q-card >
+    <q-table
+      v-bind="$attrs"
+      ref="table"
+      v-model:selected="table.selected"
+      v-model:pagination="table.pagination"
+      selection="multiple"
+      :rows="getLexiconData"
+      row-key="key"
+      binary-state-sort
+      :rows-per-page-options="[50]"
+      :columns="table.fields"
+      :visible-columns="table.visibleColumns"
+      card-class="shadow-8"
+      table-style="max-height:80vh"
+      :loading="lexiconLoading"
+      loading-label="loading"
+      :filter="table.filter"
+      :class="($q.dark.isActive ? 'my-sticky-header-table-dark' : 'my-sticky-header-table') + ' rounded-borders'"
+      @row-click="onRowClick"
+    >
+      <template #header-selection="scope">
+        <q-checkbox v-model="scope.selected" />
+      </template>
 
-    <template #body-selection="scope">
-      <q-checkbox v-model="scope.selected" />
-    </template>
+      <template #body-selection="scope">
+        <q-checkbox v-model="scope.selected" />
+      </template>
 
-    <template #body-cell-form="props">
-      <q-td key="form" :props="props">
-        <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.form }} </span>
-        <template v-if="compareWithBefore">
-          <br />
-          <span class="removed-prop">
-            <del>
-              {{ findOriginalLexiconItem(props.row).feats.form }}
-            </del>
-          </span>
-        </template>
-      </q-td>
-    </template>
-
-    <template #body-cell-lemma="props">
-      <q-td key="lemma" :props="props">
-        <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.lemma }} </span>
-        <template v-if="compareWithBefore">
-          <br />
-          <span class="removed-prop">
-            <del>
-              {{ findOriginalLexiconItem(props.row).feats.lemma }}
-            </del>
-          </span>
-        </template>
-      </q-td>
-    </template>
-
-    <template #body-cell-pos="props">
-      <q-td key="pos" :props="props">
-        <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.upos }} </span>
-        <template v-if="compareWithBefore">
-          <br />
-          <span class="removed-prop">
-            <del>
-              {{ findOriginalLexiconItem(props.row).feats.upos }}
-            </del>
-          </span>
-        </template>
-      </q-td>
-    </template>
-    <template #body-cell-gloss="props">
-      <q-td key="gloss" :props="props">
-        <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.gloss }} </span>
-        <template v-if="compareWithBefore">
-          <br />
-          <span class="removed-prop">
-            <del>
-              {{ findOriginalLexiconItem(props.row).Gloss }}
-            </del>
-          </span>
-        </template>
-      </q-td>
-    </template>
-    <!-- <template #body-cell-features="props">
-      <q-td key="features" :props="props">
-        <span :class="compareWithBefore ? 'added-prop' : ''"> {{ computeFeatureStringWrapper(props.row.features) }} </span>
-        <template >
-          <br />
-          <span class="removed-prop">
-            <del>
-              {{ computeFeatureStringWrapper(findOriginalLexiconItem(props.row).features) }}
-            </del>
-          </span>
-        </template>
-      </q-td>
-    </template> -->
-    <template #body-cell-frequency="props">
-      <q-td key="frequency" :props="props">
-        {{ props.row.frequency }}
-      </q-td>
-    </template>
-
-    <template #top-right>
-      <div>
-        <q-input v-model="table.filter" borderless dense debounce="300" placeholder="Search">
-          <template #append>
-            <q-icon name="search" />
+      <template #body-cell-form="props">
+        <q-td key="form" :props="props">
+          <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.form }} </span>
+          <template v-if="compareWithBefore">
+            <br />
+            <span class="removed-prop">
+              <del>
+                {{ findOriginalLexiconItem(props.row).feats.form }}
+              </del>
+            </span>
           </template>
-        </q-input>
-      </div>
-      <q-btn color="default" flat label="tsv" @click="exportLexiconTSV">
-       <q-tooltip :delay="300" content-class="text-white bg-primary">{{ $t('projectView.tooltipExportLexicon[0]') }}</q-tooltip>
-       </q-btn>
-       <q-btn color="default" flat label="json" @click="exportLexiconJSON" >
-       <q-tooltip :delay="300" content-class="text-white bg-primary">{{ $t('projectView.tooltipExportLexicon[1]') }}
-       </q-tooltip>
-       </q-btn>
-      <div>
-        <q-btn-group v-if="compareWithBefore" flat>
-          <q-btn color="default" :disable="table.selected.length === 0" flat icon="compare_arrows" @click="get()"
-            ><q-tooltip>Generate Grew Rule</q-tooltip></q-btn
-          >
-          <q-btn color="default" :disable="table.selected.length === 0" flat icon-right="delete_forever" @click="deleteSelected()"
-            ><q-tooltip>Unstage selected lexicon changes</q-tooltip></q-btn
-          >
-        </q-btn-group>
-      </div>
-    </template>
+        </q-td>
+      </template>
 
+      <template #body-cell-lemma="props">
+        <q-td key="lemma" :props="props">
+          <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.lemma }} </span>
+          <template v-if="compareWithBefore">
+            <br />
+            <span class="removed-prop">
+              <del>
+                {{ findOriginalLexiconItem(props.row).feats.lemma }}
+              </del>
+            </span>
+          </template>
+        </q-td>
+      </template>
+
+      <template #body-cell-pos="props">
+        <q-td key="pos" :props="props">
+          <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.upos }} </span>
+          <template v-if="compareWithBefore">
+            <br />
+            <span class="removed-prop">
+              <del>
+                {{ findOriginalLexiconItem(props.row).feats.upos }}
+              </del>
+            </span>
+          </template>
+        </q-td>
+      </template>
+      <template #body-cell-gloss="props">
+        <q-td key="gloss" :props="props">
+          <span :class="compareWithBefore && passedLexiconItems.length >= 1 ? 'added-prop' : ''"> {{ props.row.gloss }} </span>
+          <template v-if="compareWithBefore">
+            <br />
+            <span class="removed-prop">
+              <del>
+                {{ findOriginalLexiconItem(props.row).Gloss }}
+              </del>
+            </span>
+          </template>
+        </q-td>
+      </template>
+      <!-- <template #body-cell-features="props">
+        <q-td key="features" :props="props">
+          <span :class="compareWithBefore ? 'added-prop' : ''"> {{ computeFeatureStringWrapper(props.row.features) }} </span>
+          <template >
+            <br />
+            <span class="removed-prop">
+              <del>
+                {{ computeFeatureStringWrapper(findOriginalLexiconItem(props.row).features) }}
+              </del>
+            </span>
+          </template>
+        </q-td>
+      </template> -->
+      <template #body-cell-frequency="props">
+        <q-td key="frequency" :props="props">
+        <q-btn           
+        dense
+        color="secondary"
+        outline
+        :label=" props.row.frequency"
+        @click.stop="showTrees(props.row)" />
+        </q-td>
+      </template>
+
+      <template #top-right>
+        <div>
+          <q-input v-model="table.filter" borderless dense debounce="300" placeholder="Search">
+            <template #append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </div>
+        <q-btn color="default" flat label="tsv" @click="exportLexiconTSV">
+        <q-tooltip :delay="300" content-class="text-white bg-primary">{{ $t('projectView.tooltipExportLexicon[0]') }}</q-tooltip>
+        </q-btn>
+        <q-btn color="default" flat label="json" @click="exportLexiconJSON" >
+        <q-tooltip :delay="300" content-class="text-white bg-primary">{{ $t('projectView.tooltipExportLexicon[1]') }}
+        </q-tooltip>
+        </q-btn>
+        <div>
+          <q-btn-group v-if="compareWithBefore" flat>
+            <q-btn color="default" :disable="table.selected.length === 0" flat icon="compare_arrows" @click="get()"
+              ><q-tooltip>Generate Grew Rule</q-tooltip></q-btn
+            >
+            <q-btn color="default" :disable="table.selected.length === 0" flat icon-right="delete_forever" @click="deleteSelected()"
+              ><q-tooltip>Unstage selected lexicon changes</q-tooltip></q-btn
+            >
+          </q-btn-group>
+        </div>
+      </template>
+    
     <!-- Dynamically inherit slots from parent -->
-    <template v-for="slot in parentSlots" #[slot]>
-      <slot :name="slot" />
-    </template>
+      <template v-for="slot in parentSlots" #[slot]>
+        <slot :name="slot" />
+      </template>
   </q-table>
+  <q-card-section>
+    <q-dialog v-model="visuTreeDial" maximized transition-show="fade" transition-hide="fade">
+    <ResultView
+      :searchresults="resultSearch"
+      :searchscope="projectName"
+    ></ResultView>
+  </q-dialog>
+  </q-card-section>
+
+</q-card>
 </template>
 
 <script lang="ts">
 import api from '../../api/backend-api';
 import { computed } from 'vue';
 import { mapActions, mapState } from 'pinia';
+import ResultView from '../ResultView.vue';
 import { lexiconItem_FE_t, useLexiconStore } from 'src/pinia/modules/lexicon';
 import { useGrewSearchStore } from 'src/pinia/modules/grewSearch';
 import { table_t } from 'src/types/main_types';
 import { notifyError, notifyMessage } from 'src/utils/notify';
 import { defineComponent } from 'vue';
+import { grewSearchResult_t } from 'src/api/backend-types';
 
 export default defineComponent({
+  components: { ResultView},
   name: 'LexiconTable',
   props: ['passedLexiconItems', 'lexiconLoading', 'compareWithBefore'],
   setup(props, ctx) {
@@ -153,6 +172,7 @@ export default defineComponent({
     return { parentSlots };
   },
   data() {
+    const resultSearch: grewSearchResult_t = {};
     const lexiconData :{
                     form :string,
                     lemma:string,
@@ -173,9 +193,6 @@ export default defineComponent({
           name: 'form',
           label: 'Form',
           sortable: true,
-          // sort: (a: string, b: string) => {
-          //   return a.localeCompare(b);
-          // },
           align: 'left',
           field: 'form',
         },
@@ -192,11 +209,6 @@ export default defineComponent({
           sortable: true,
           align: 'left',
           field: 'upos',
-          // Add a sort function for handling the requested sorting feature
-          // sort: (a: string, b: string) => {
-          //   console.log('KK a', a, b);
-          //   return a.localeCompare(b);
-          // },
         },
         // {
         //   name: 'features',
@@ -234,6 +246,8 @@ export default defineComponent({
       exporting: false,
     };
     return {
+      resultSearch,
+      visuTreeDial: false,
       lexiconData,
       download: [],
       table,
@@ -258,7 +272,9 @@ export default defineComponent({
       
       return this.lexiconData;
     },
-
+     projectName() {
+      return this.$route.params.projectname;
+    },
   },
   methods: {
     ...mapActions(useLexiconStore, ['setLexiconModificationItem', 'removeCoupleLexiconItemBeforeAfter']),
@@ -300,7 +316,7 @@ export default defineComponent({
     grew_pattern_from_lex_item(lex_item: lexiconItem_FE_t) {
       let pattern = 'pattern { N[';
       for (const [feat, value] of Object.entries(lex_item.feats)) {
-        if (lex_item.feats) {
+        if (feat && value) {
           pattern += `${feat} = \"${value}\", `;
         } else {
           pattern += `!${feat}, `;
@@ -326,7 +342,9 @@ export default defineComponent({
       commands += '}';
       return this.grew_pattern_from_lex_item(before) + withouts + '\n' + commands;
     },
-
+     showTrees(row: any){
+     this.onSearch(this.grew_pattern_from_lex_item(this.findOriginalLexiconItem(row)))
+     },
      exportLexiconTSV() {
       const download=[];
        for ( const lexiconItem of this.passedLexiconItems) {
@@ -378,8 +396,21 @@ export default defineComponent({
          });
        this.download = [];
      },
+     onSearch(searchPattern: string) {
+      const query = { pattern: searchPattern };
+        api
+          .searchProject(this.$route.params.projectname as string, query)
+          .then((response) => {
+            this.resultSearch = response.data;            
+            this.visuTreeDial = true;
+          })
+          .catch((error) => {
+            notifyError({ error });
+          });
+      }
+    },
   },
-});
+);
 </script>
 
 <style>

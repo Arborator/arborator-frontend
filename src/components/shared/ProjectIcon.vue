aa529f48cf2f09a63234e5e587b7e9baf09245b1
kirianguiller <kirianguiller@gmail.com>
2023-02-19 12:24:29 -0500

    style: light theme colors for ProjectIcon.vue

--- src/components/shared/ProjectIcon.vue
+++ src/components/shared/ProjectIcon.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-icon v-show="visibility === 0" name="lock" :color="$q.dark.isActive ? 'red-6' : 'negative'" size="lg"></q-icon>
-  <q-icon v-show="visibility === 1" name="visibility" :color="$q.dark.isActive ? 'yellow-5' : 'positive'" size="lg"></q-icon>
+  <q-icon v-show="visibility === 0" name="lock" :color="$q.dark.isActive ? 'red-6' : 'red-14'" size="lg"></q-icon>
+  <q-icon v-show="visibility === 1" name="visibility" :color="$q.dark.isActive ? 'yellow-5' : 'yellow-8'" size="lg"></q-icon>
   <q-icon v-show="visibility === 2" name="groups" :color="$q.dark.isActive ? 'light-green-12' : 'positive'" size="lg"></q-icon>
   <q-icon v-show="exerciseMode" name="school" color="indigo-11" size="lg"></q-icon>
 </template>

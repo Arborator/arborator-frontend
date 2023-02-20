612deb976383c8d3ef1f5e4103b89b7b2696d3c4
kirianguiller <kirianguiller@gmail.com>
2023-02-20 12:55:16 -0500

    add: minor changes to 53d04901; apply rule logic #190

--- src/components/ResultView.vue
+++ src/components/ResultView.vue
@@ -1,37 +1,39 @@
 <template>
   <q-card style="max-width: 100vw">
     <q-bar class="bg-primary text-white">
-      <q-icon name="img:/svg/grew.svg" size="7rem" />
-      <q-space />
+      <q-icon name="img:/svg/grew.svg" size="7rem"/>
+      <q-space/>
       <div class="text-weight-bold">
-        {{ sentenceCount }} <span v-if="sentenceCount === 1">result</span><span v-else>results</span> of the {{ totalsents }}
+        {{ sentenceCount }} <span v-if="sentenceCount === 1">result</span><span v-else>results</span> of the
+        {{ totalsents }}
         <span v-if="totalsents === 1">sentence</span><span v-else>sentences</span> in the {{ searchscope }}
       </div>
-      <q-space />
-      <q-btn v-close-popup flat dense icon="close" />
+      <q-space/>
+      <q-btn v-close-popup flat dense icon="close"/>
     </q-bar>
-    <div v-if="queryType === 'REWRITE' && samplesFrozen.list.length > 0 && (isGuest || isAdmin || isSuperAdmin)" class="q-pa-md">
-      <q-btn color="primary" label="Apply rules" @click="save" />
+    <div v-if="queryType === 'REWRITE' && samplesFrozen.list.length > 0 && (isGuest || isAdmin || isSuperAdmin)"
+         class="q-pa-md">
+      <q-btn color="primary" label="Apply rules" @click="applyRules"/>
     </div>
     <q-card-section>
       <div v-show="!loading" class="q-pa-md row q-gutter-md">
         <div v-if="samplesFrozen.list.length > 0">
           <q-virtual-scroll
-            :items="samplesFrozen.list"
-            style="height: 80vh; width: 100vw"
-            :virtual-scroll-slice-size="5"
-            :virtual-scroll-item-size="200"
-            type="list"
+              :items="samplesFrozen.list"
+              style="height: 80vh; width: 100vw"
+              :virtual-scroll-slice-size="5"
+              :virtual-scroll-item-size="200"
+              type="list"
           >
             <template #default="{ item, index }">
               <SentenceCard
-                :id="item[1]"
-                :key="index"
-                :sentence="searchresults[item[0]][item[1]]"
-                :index="index"
-                :sentence-id="item[1]"
-                :matches="searchresults[item[0]][item[1]]"
-                :exercise-level="4"
+                  :id="item[1]"
+                  :key="index"
+                  :sentence="searchresults[item[0]][item[1]]"
+                  :index="index"
+                  :sentence-id="item[1]"
+                  :matches="searchresults[item[0]][item[1]]"
+                  :exercise-level="4"
               ></SentenceCard>
             </template>
           </q-virtual-scroll>
@@ -39,28 +41,28 @@
       </div>
       <div v-show="loading" class="q-pa-md row justify-center">
         <div class="col">
-          <q-circular-progress indeterminate size="70px" :thickness="0.22" color="primary" :track-color="$q.dark.isActive ? 'grey' : 'grey-3'" />
+          <q-circular-progress indeterminate size="70px" :thickness="0.22" color="primary"
+                               :track-color="$q.dark.isActive ? 'grey' : 'grey-3'"/>
         </div>
       </div>
     </q-card-section>
   </q-card>
 </template>
 
+
 <script lang="ts">
 import api from '../api/backend-api';
 import SentenceCard from './sentence/SentenceCard.vue';
-import conllup from 'conllup';
-const sentenceConllToJson = conllup.sentenceConllToJson;
-const sentenceJsonToConll = conllup.sentenceJsonToConll;
-import { useUserStore } from 'src/pinia/modules/user';
-import { useProjectStore } from 'src/pinia/modules/project';
-import { mapState } from 'pinia';
-import { PropType, defineComponent } from 'vue';
-import { notifyError, notifyMessage } from 'src/utils/notify';
-import { grewSearchResult_t, sample_t } from 'src/api/backend-types';
+import {useUserStore} from 'src/pinia/modules/user';
+import {useProjectStore} from 'src/pinia/modules/project';
+import {mapState} from 'pinia';
+import {PropType, defineComponent} from 'vue';
+import {notifyMessage} from 'src/utils/notify';
+import {grewSearchResult_t, sample_t} from 'src/api/backend-types';
+import {sentenceConllToJson, sentenceJsonToConll} from "conllup/lib/conll";
 
 export default defineComponent({
-  components: { SentenceCard },
+  components: {SentenceCard},
   props: {
     searchresults: {
       type: Object as PropType<grewSearchResult_t>,
@@ -91,7 +93,7 @@ export default defineComponent({
       indexes: { [key: number]: string[] };
       samples: sample_t[];
       selected: { [key: number]: boolean };
-    } = { list: [], indexes: {}, samples: [], selected: {} };
+    } = {list: [], indexes: {}, samples: [], selected: {}};
 
     return {
       searchresultsCopy,
@@ -105,11 +107,11 @@ export default defineComponent({
   },
   computed: {
     ...mapState(useProjectStore, ['isGuest', 'isAdmin']),
-    ...mapState(useUserStore, ['isSuperAdmin', 'getUserInfos']),
+    ...mapState(useUserStore, ['isSuperAdmin', 'username']),
     sentenceCount() {
       return Object.keys(this.searchresults)
-        .map((sa) => Object.keys(this.searchresults[sa]))
-        .flat().length;
+          .map((sa) => Object.keys(this.searchresults[sa]))
+          .flat().length;
       // number of keys in subobjects
     },
   },
@@ -124,7 +126,6 @@ export default defineComponent({
      * @returns void
      */
     freezeSamples() {
-      // console.log('samples to freeze', JSON.stringify(this.searchResults) );
       const listIds = []; // list: [["WAZA_10_Bluetooth-Lifestory_MG","WAZA_10_Bluetooth-Lifestory_MG__86"],["WAZA_10_Bluetooth-Lifestory_MG","WAZA_10_Bluetooth-Lifestory_MG__79"], ...
       let index = 0;
       const index2Ids: { [key: number]: string[] } = {}; // object: {"0":["WAZA_10_Bluetooth-Lifestory_MG","WAZA_10_Bluetooth-Lifestory_MG__86"],"1":["WAZA_10_Bluetooth-Lifestory_MG","WAZA_10_Bluetooth-Lifestory_MG__79"], ...
@@ -140,7 +141,6 @@ export default defineComponent({
           index += 1;
         }
       }
-      // heavyList = listIds;
       Object.freeze(listIds);
       this.samplesFrozen = {
         list: listIds,
@@ -148,60 +148,49 @@ export default defineComponent({
         selected: selectedIndex,
         samples: JSON.parse(JSON.stringify(this.samples)),
       };
-      // console.log(this.samplesFrozen)
     },
-    /**
-     * Requests project configuration from backend and put it into the store
-     *
-     * @returns void
-     * removed by kirian 2020_08_04 22h59
-     */
-    // getProjectConfig(){
-    //     api.getProjectSettings(this.$route.params.projectname)
-    //     .then(response => {
-    //         this.$store.commit('config/set_project_config', response.data);
-    //     })
-    //     .catch(error => {
-    //         this.$store.dispatch("notifyError", {error: error});
-    //     });
-    // },
     /**
      * Save the graph to backend after modifying its metadata and changing it into an object
      *
      * @returns void
      */
-    save() {
+    applyRules() {
       let sentenceJson;
       let conll;
+      let toSaveCounter = 0;
       this.searchresultsCopy = this.searchresults;
-      for (const sample in this.searchresults){
-        for (const sentId in this.searchresults[sample]){
-          if (!this.searchresults[sample][sentId].conlls[this.getUserInfos.username]){
-            
-            sentenceJson= sentenceConllToJson(Object.values(this.searchresults[sample][sentId].conlls)[0])
-            sentenceJson.metaJson.user_id = this.getUserInfos.username
+      for (const sample in this.searchresults) {
+        for (const sentId in this.searchresults[sample]) {
+          if (!this.searchresults[sample][sentId].conlls[this.username]) {
+
+            sentenceJson = sentenceConllToJson(Object.values(this.searchresults[sample][sentId].conlls)[0])
+            sentenceJson.metaJson.user_id = this.username
             sentenceJson.metaJson.timestamp = Math.round(Date.now())
             conll = sentenceJsonToConll(sentenceJson)
-            this.searchresultsCopy[sample][sentId].conlls[this.getUserInfos.username] = conll
-            for (const userId in this.searchresultsCopy[sample][sentId].conlls){
-              if (userId !== this.getUserInfos.username ) delete this.searchresultsCopy[sample][sentId].conlls[userId]
+            this.searchresultsCopy[sample][sentId].conlls[this.username] = conll
+            for (const userId in this.searchresultsCopy[sample][sentId].conlls) {
+              if (userId !== this.username) delete this.searchresultsCopy[sample][sentId].conlls[userId]
             }
-            console.log(this.searchresultsCopy[sample][sentId].conlls)
+            toSaveCounter += 1;
+
 
           }
         }
       }
-      if (Object.keys(this.searchresultsCopy).length !== 0) {
-        const datasample = { data: this.searchresultsCopy };
+      if (toSaveCounter >= 1) {
+        const datasample = {data: this.searchresultsCopy};
         api
-          .applyRule(this.$route.params.projectname as string, datasample)
-          .then(() => {
-          this.resultSearchDialog = false;
-          this.parentOnShowTable(this.resultSearchDialog);
-          notifyMessage({ message: 'Rule applied' });
-        });
+            .applyRule(this.$route.params.projectname as string, datasample)
+            .then(() => {
+              this.resultSearchDialog = false;
+              this.parentOnShowTable(this.resultSearchDialog);
+              notifyMessage({message: `Rule applied (user "${this.username}" rewrote and saved "${toSaveCounter}" at once)`});
+            });
       } else {
-        console.log('not ok');
+        notifyMessage({
+          message: `Nothing to save (user "${this.username}" has "zero" tree matching rewriting pattern)`,
+          type: "warning"
+        });
       }
     },
   },

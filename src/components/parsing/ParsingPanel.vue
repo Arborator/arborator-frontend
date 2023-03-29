34d41753ee99db5a168d3d3e7f6e325f590d69a0
kirianguiller <kirianguiller@gmail.com>
2023-03-29 19:39:11 +0200

    add: use pretrained model for train or parse

--- src/components/parsing/ParsingPanel.vue
+++ src/components/parsing/ParsingPanel.vue
@@ -4,16 +4,33 @@
       <div class="col">
         <p><b>General Settings</b></p>
         <div style="display: flex; flex-direction: column">
-          <q-radio v-model="param.pipelineChoice" val="TRAIN_AND_PARSE" label="Train and Parse"/>
-          <q-radio v-model="param.pipelineChoice" val="TRAIN_ONLY" label="Train Only"/>
-          <q-radio v-model="param.pipelineChoice" val="PARSE_ONLY" label="Parse Only"/>
+          <q-option-group
+            :disable="taskStatus !== null"
+            :options="param.pipelineOptions"
+            type="radio"
+            v-model="param.pipelineChoice"
+          />
         </div>
+        <q-select
+          :disable="taskStatus !== null"
+          v-model="param.baseModel"
+          filled
+          :options="param.baseModelsOptions"
+          :label="`Base Model for training or parsing`"
+          style="max-width: 200px; min-width: 150px"
+<!--          :rules="[()=>{-->
+<!--            if (noSelectedBaseModelForParsingError()) {-->
+<!--              return `OTOTOTOTO`-->
+<!--            }-->
+<!--          }]"-->
+        />
       </div>
       <q-separator vertical inset class="q-mx-lg"/>
       <div v-show="param.pipelineChoice !== 'PARSE_ONLY'" class="col">
         <p><b>Train Settings</b></p>
-        <q-toggle v-model="param.trainAll" label="Train on all files"/>
+        <q-toggle :disable="taskStatus !== null" v-model="param.trainAll" label="Train on all files"/>
         <q-select
+          :disable="taskStatus !== null"
           :class="{ invisible: param.trainAll }"
           v-model="param.trainSamples"
           filled
@@ -23,8 +40,9 @@
           stack-label
           style="max-width: 200px; min-width: 150px"
         />
-        <q-toggle v-model="param.isCustomTrainingUser" label="Custom Training user"/>
+        <q-toggle :disable="taskStatus !== null" v-model="param.isCustomTrainingUser" label="Custom Training user"/>
         <q-select
+          :disable="taskStatus !== null"
           :class="{ invisible: !param.isCustomTrainingUser }"
           v-model="param.trainingUser"
           filled
@@ -34,6 +52,7 @@
           style="max-width: 200px; min-width: 150px"
         />
         <q-input
+          :disable="taskStatus !== null"
           v-model.number="param.epochs"
           type="number"
           label="epochs"
@@ -47,8 +66,9 @@
       <q-separator v-show="param.pipelineChoice !== 'PARSE_ONLY'" vertical inset class="q-mx-lg"/>
       <div v-show="param.pipelineChoice !== 'TRAIN_ONLY'" class="col">
         <p><b>Parse Settings</b></p>
-        <q-toggle v-model="param.parseAll" label="Parse all files"/>
+        <q-toggle :disable="taskStatus !== null" v-model="param.parseAll" label="Parse all files"/>
         <q-select
+          :disable="taskStatus !== null"
           :class="{ invisible: param.parseAll }"
           v-model="param.parseSamples"
           filled
@@ -58,19 +78,28 @@
           stack-label
           style="max-width: 200px; min-width: 150px"
         />
-        <q-input :dense="true" v-model="param.parserSuffix" label="Parser suffix (for parsed sentences)"
+        <q-input :disable="taskStatus !== null" :dense="true" v-model="param.parserSuffix"
+                 label="Parser suffix (for parsed sentences)"
                  :hint="'Parsing will go under the name `parser' + param.parserSuffix + '`'"/>
-<!--        <q-toggle v-model="param.keepUpos" label="keep UPOS"/>-->
+        <!--        <q-toggle v-model="param.keepUpos" label="keep UPOS"/>-->
       </div>
       <q-separator v-show="param.pipelineChoice !== 'TRAIN_ONLY'" vertical inset class="q-mx-lg"/>
       <div class="col">
-        <p><b>Start Pipeline</b></p>
+        <p><b>Pipeline Summary</b></p>
+        <div class="text-subtitle5 q-mb-xs">training sentences : {{ trainingSentencesCount }}</div>
+        <div class="text-subtitle5 q-mb-xs">parsing sentences : {{ parsingSentencesCount }}</div>
         <div class="text-subtitle5 q-mb-xs">estimated time = {{ estimatedTime }}mn</div>
-        <div class="text-subtitle5 q-mb-xs">({{ trainingSentencesCount }} training sentences)</div>
-        <q-btn v-close-popup color="primary" label="START" :loading="taskStatus !== null" push size="sm"
-               @click="parserTrainStart()"/>
+        <q-btn v-close-popup color="primary" :disable="paramError" label="START" :loading="taskStatus !== null" push
+               size="sm"
+               @click="parserPipelineStart()"/>
         <!--        <q-btn v-if="parsing" v-close-popup color="primary" label="Stop" push size="sm"-->
         <!--               @click="bootParserStop()"/>-->
+        <div v-if="taskStatus !== null">
+          <div class="text-subtitle5 q-mb-xs">Current task : {{ taskStatus.taskType }}</div>
+          <div v-if="taskStatus.additionalInfo" class="text-subtitle5 q-mb-xs">Additional info :
+            {{ taskStatus.additionalInfo }}
+          </div>
+        </div>
       </div>
     </div>
   </QCard>
@@ -95,11 +124,21 @@ type taskStatus_t = null | {
   taskIntervalChecker: null | ReturnType<typeof setTimeout>,
 }
 
+type pipelineChoice_t = "TRAIN_AND_PARSE" | "TRAIN_ONLY" | "PARSE_ONLY";
+
+type modelInfo_t = {
+  project_name: string;
+  model_id: string;
+}
+
 interface parser_t {
   progress: string;
   taskStatus: taskStatus_t;
   param: {
-    pipelineChoice: "TRAIN_AND_PARSE" | "TRAIN_ONLY" | "PARSE_ONLY";
+    pipelineChoice: pipelineChoice_t;
+    pipelineOptions: { label: string; value: pipelineChoice_t }[];
+    baseModel: modelInfo_t | null;
+    baseModelsOptions: { label: string; value: modelInfo_t }[];
     advancedSettings: boolean;
     keepUpos: boolean;
     isCustomTrainingUser: boolean;
@@ -135,6 +174,13 @@ export default defineComponent({
       progress: 'bootstrap parsing',
       param: {
         pipelineChoice: 'TRAIN_AND_PARSE',
+        pipelineOptions: [
+          {label: "Train and Parse", value: "TRAIN_AND_PARSE"},
+          {label: "Train Only", value: "TRAIN_ONLY"},
+          {label: "Parse Only", value: "PARSE_ONLY"},
+        ],
+        baseModel: null,
+        baseModelsOptions: [],
         advancedSettings: false,
         keepUpos: false,
         isCustomTrainingUser: false,
@@ -148,6 +194,9 @@ export default defineComponent({
       },
     };
     return data;
+  },
+  mounted() {
+    this.fetchBaseModelsAvailables();
   },
   computed: {
     allSamplesNames() {
@@ -167,16 +216,70 @@ export default defineComponent({
         .filter((sample) => this.param.trainSamples.includes(sample.sample_name))
         .reduce((partialSum, sample) => partialSum + sample.sentences, 0);
     },
+    parsingSentencesCount() {
+      if (this.param.parseAll) {
+        return this.samples.map((sample) => sample.sentences).reduce((partialSum, a) => partialSum + a, 0);
+      }
+      return this.samples
+        .filter((sample) => this.param.parseSamples.includes(sample.sample_name))
+        .reduce((partialSum, sample) => partialSum + sample.sentences, 0);
+    },
     estimatedTime() {
-      const x = this.trainingSentencesCount;
-      const timeInitialisationTask_s = 30;
-      const trainingEstimatedTime_s = this.param.epochs * (x / kirParserSentPerSecSpeed);
-      const parsingEstimatedTime_s = 60;
-      const totalEstimatedTime_s = timeInitialisationTask_s + trainingEstimatedTime_s + parsingEstimatedTime_s
+      const timeInitialisationTrainingTask_s = 30;
+      const trainingEstimatedTime_s = this.param.epochs * (0.5 + this.trainingSentencesCount / kirParserSentPerSecSpeed);
+      const timeInitialisationParsingTask_s = 30;
+      const parsingEstimatedTime_s = this.parsingSentencesCount / kirParserSentPerSecSpeed;
+      const totalEstimatedTime_s = timeInitialisationTrainingTask_s + trainingEstimatedTime_s + timeInitialisationParsingTask_s + parsingEstimatedTime_s
       return Math.ceil(totalEstimatedTime_s / 60);
+    },
+    noTrainFileSelectedError() {
+      return (this.param.pipelineChoice !== "PARSE_ONLY" && this.param.trainAll === false && this.param.trainSamples.length === 0);
+    },
+    noParseFileSelectedError() {
+      return (this.param.pipelineChoice !== "TRAIN_ONLY" && this.param.parseAll === false && this.param.parseSamples.length === 0);
+    },
+    noSelectedBaseModelForParsingError() {
+      return (this.param.pipelineChoice === "PARSE_ONLY" && !this.param.baseModel)
+    },
+    paramError() {
+      if (this.noTrainFileSelectedError) {
+        return true
+      }
+      if (this.noParseFileSelectedError) {
+        return true
+      }
+      if (this.noSelectedBaseModelForParsingError) {
+        return true
+      }
+      return false
     },
   },
   methods: {
+    fetchBaseModelsAvailables() {
+      api.parserList().then(
+        (response) => {
+          if (response.data.status === "failure") {
+            console.log("fetchBaseModelsAvailables FAILURE")
+          } else {
+            this.param.baseModelsOptions = response.data.data.map((baseModel: modelInfo_t) => {
+              return {
+                label: `${baseModel.project_name}/${baseModel.model_id}`,
+                value: baseModel
+              }
+            })
+          }
+        }
+      )
+    },
+    parserPipelineStart() {
+      if (this.param.pipelineChoice === "TRAIN_AND_PARSE" || this.param.pipelineChoice === "TRAIN_ONLY") {
+        this.parserTrainStart();
+      } else {
+        if (this.param.baseModel) {
+          this.parserParseStart(this.param.baseModel);
+        }
+      }
+    },
     parserTrainStart() {
       this.taskStatus = {
         taskType: "ASK_TRAINING",
@@ -215,16 +318,19 @@ export default defineComponent({
           if (response.data.status === "failure") {
             this.clearCurrentTask()
           } else if (response.data.data.task_status === "READY") {
-            this.clearCurrentTask()
-            this.parserParseStart(modelInfo)
             notifyMessage({message: "Model training ended!"})
+            this.clearCurrentTask()
+            this.fetchBaseModelsAvailables()
+            if (this.param.pipelineChoice === "TRAIN_AND_PARSE") {
+              this.parserParseStart(modelInfo)
+            }
           } else if (this.taskStatus && Date.now() - this.taskStatus.taskTimeStarted > TIMEOUT_TASK_STATUS_CHECKER) {
             this.clearCurrentTask()
           }
         }
       )
     },
-    parserParseStart(modelInfo: { project_name: string; model_id: string }) {
+    parserParseStart(modelInfo: modelInfo_t) {
       this.taskStatus = {
         taskType: "ASK_PARSING",
         taskTimeStarted: Date.now(),

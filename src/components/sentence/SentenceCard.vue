1857586fdad56953c47bf578eb8c05d506cb697a
kirianguiller <kirianguiller@gmail.com>
2023-06-19 15:24:35 +0200

    Revert "add: better information for sentence idx and id"
    This reverts commit e56a76c16fe9de662331418c1ce2ac24904a9648.

--- src/components/sentence/SentenceCard.vue
+++ src/components/sentence/SentenceCard.vue
@@ -2,13 +2,10 @@
   <q-card :id="index">
     <q-card-section>
       <div class="row items-center">
-        <span class="text-center" :color="$q.dark.isActive ? 'grey' : ''" dense>
-          {{ index + 1 }}
-          <q-tooltip>
-            sent_id = {{ sentenceId }}
-          </q-tooltip>
-        </span>
-
+        <span class="text-grey">{{ index + 1 }}</span>
+        <q-chip class="text-center" :color="$q.dark.isActive ? 'grey' : ''" dense> {{ sentenceId }}
+        </q-chip
+        >&nbsp;&nbsp;&nbsp;
         <q-input
           v-model="sentenceData.sentence"
           style="width: 65%"
@@ -199,7 +196,7 @@
         </q-tab-panel>
       </q-tab-panels>
       <q-list v-if="openTabUser" class="sentence__meta-features" dense>
-        <q-item v-for="meta in (shownMeta.length > 0 ? shownMeta : Object.keys(reactiveSentencesObj[openTabUser].state.metaJson))" :key="meta">
+        <q-item v-for="meta in shownMeta" :key="meta">
           <q-chip dense size="xs">{{ meta }}
           </q-chip
           >

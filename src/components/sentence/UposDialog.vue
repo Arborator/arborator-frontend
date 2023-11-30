60d99888baf6e09ccc7aea1b66cace41f6d18d30
khansadaoudi <hk_daoudi@esi.dz>
2023-11-30 11:25:09 +0100

    refactor upos and xpos dialogs

--- src/components/sentence/UposDialog.vue
+++ src/components/sentence/UposDialog.vue
@@ -1,118 +1,33 @@
 <template>
-  <!-------------------- Start uposDialog -------------------->
-  <q-dialog v-model="uposDialogOpened" @keyup.enter="onChangeUpos()">
-    <q-card style="min-width: 700px">
-      <q-bar class="bg-primary text-white">
-        <div class="text-weight-bold">UPOS : {{ $t('attributeTable.category[0]') }} "{{ token.FORM }}"</div>
-        <q-space />
-        <q-btn v-close-popup flat dense icon="close" />
-      </q-bar>
-      <q-card-section>
-        <div class="q-gutter-sm">
-          <q-chip
-            outline
-            v-for="(upos, index) in annotationFeatures.UPOS"
-            v-model:selected="selectedUpos[index]"
-            :label="upos"
-            color="primary"
-            @update:selected="unselectOtherUpos(index)"
-          />
-        </div>
-      </q-card-section>
-      <q-separator />
-      <q-card-actions>
-        <q-btn
-          v-close-popup
-          outline
-          color="primary"
-          :label="$t('delete')"
-          style="width: 25%; margin-left: auto; margin-right: auto"
-          @click="onDeleteUpos()"
-        />
-        <q-space />
-        <q-btn
-          id="catselectvalidate"
-          v-close-popup
-          color="primary"
-          label="Ok"
-          style="width: 25%; margin-left: auto; margin-right: auto"
-          @click="onChangeUpos()"
-        />
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
-
-  <!----------------- End uposDialog ------------------->
+  <PosDialog
+    :sentence-bus="sentenceBus"
+    :title="'UPOS'"
+    :pos-options="annotationFeatures.UPOS"
+    :event-name="'open:uposDialog'"
+  />
 </template>
 
 <script lang="ts">
-import { PropType } from 'vue';
-import { sentence_bus_t } from 'src/types/main_types';
-import { useProjectStore } from 'src/pinia/modules/project';
 import { mapState } from 'pinia';
-import conllup from 'conllup';
-const emptyTokenJson = conllup.emptyTokenJson;
-import { defineComponent } from 'vue';
+import { useProjectStore } from 'src/pinia/modules/project';
+import { sentence_bus_t } from 'src/types/main_types';
+
+import { PropType, defineComponent } from 'vue';
+import PosDialog from './PosDialog.vue';
 
 export default defineComponent({
+  name: 'UposDialog',
+  components: {
+    PosDialog,
+  },
   props: {
     sentenceBus: {
       type: Object as PropType<sentence_bus_t>,
       required: true,
     },
   },
-  data() {
-    const token = emptyTokenJson();
-    const selectedUpos: boolean[] = [];
-    return {
-      selectedUpos,
-      uposDialogOpened: false,
-      token,
-      userId: '',
-    };
-  },
   computed: {
     ...mapState(useProjectStore, ['annotationFeatures']),
-  },
-  mounted() {
-    this.sentenceBus.on('open:uposDialog', ({ token, userId }) => {
-      this.token = token;
-      this.userId = userId;
-      this.selectedUpos = this.annotationFeatures.UPOS.map((upos) => upos === this.token.UPOS)
-      this.uposDialogOpened = true;
-    });
-  },
-  beforeUnmount() {
-    this.sentenceBus.off('open:uposDialog');
-  },
-  methods: {
-    unselectOtherUpos(index: any) {
-      this.selectedUpos.forEach((upos, i) => {
-        if (upos && i != index) {
-          this.selectedUpos[i] = false;
-        }
-      });
-    },
-    onChangeUpos() {
-      if (this.selectedUpos.some((val) => val == true)) {
-        this.token.UPOS = this.annotationFeatures.UPOS[this.selectedUpos.indexOf(true)];
-      }
-      else {
-        this.token.UPOS = '_';
-      }
-      this.uposDialogOpened = false;
-      this.sentenceBus.emit('tree-update:token', {
-        token: this.token,
-        userId: this.userId,
-      });
-    },
-    onDeleteUpos() {
-      this.token.UPOS = '_';
-      this.sentenceBus.emit('tree-update:token', {
-        token: this.token,
-        userId: this.userId,
-      });
-    },
   },
 });
 </script>
--- src/components/sentence/XposDialog.vue
+++ src/components/sentence/XposDialog.vue
@@ -1,106 +1,32 @@
 <template>
-  <!-------------------- Start xposDialog -------------------->
-  <q-dialog v-model="xposDialogOpened" @keyup.enter="onChangeXpos()">
-    <!-- @hide="ondialoghide()" -->
-    <!-- :maximized="maximizedToggle" -->
-    <q-card style="height: 300px">
-      <q-bar class="bg-primary text-white">
-        <div class="text-weight-bold">XPOS : {{ $t('attributeTable.category[0]') }} "{{ token.FORM }}"</div>
-        <q-space />
-        <q-btn v-close-popup flat dense icon="close" />
-      </q-bar>
-      <q-card-section style="height: 200px">
-        <q-select
-          id="catselect"
-          v-model="token.XPOS"
-          filled
-          :options="annotationFeatures.XPOS"
-          :label="$t('attributeTable.category[1]')"
-          style="width: 250px"
-        />
-      </q-card-section>
-      <q-separator />
-      <q-card-actions>
-        <q-btn v-close-popup flat :label="$t('cancel')" style="width: 25%; margin-left: auto; margin-right: auto" />
-        <q-space />
-
-        <!-- @click="ondialoghide()" -->
-        <q-btn
-          v-close-popup
-          color="negative"
-          :label="$t('delete')"
-          style="width: 25%; margin-left: auto; margin-right: auto"
-          @click="onDeleteXpos()"
-        />
-        <q-space />
-        <q-btn
-          id="catselectvalidate"
-          v-close-popup
-          color="primary"
-          label="Ok"
-          style="width: 25%; margin-left: auto; margin-right: auto"
-          @click="onChangeXpos()"
-        />
-        <!-- :disabled="snap.currentcategory === snap.category" -->
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
-
-  <!----------------- End xposDialog ------------------->
+  <PosDialog 
+    :sentence-bus="sentenceBus"
+    :title="'XPOS'"
+    :pos-options="annotationFeatures.XPOS"
+    :event-name="'open:xposDialog'"
+  />
 </template>
 
 <script lang="ts">
-import { PropType } from 'vue';
-import { sentence_bus_t } from 'src/types/main_types';
-import { useProjectStore } from 'src/pinia/modules/project';
 import { mapState } from 'pinia';
-import conllup from 'conllup';
-const emptyTokenJson = conllup.emptyTokenJson;
-import { defineComponent } from 'vue';
+import { useProjectStore } from 'src/pinia/modules/project';
+import { sentence_bus_t } from 'src/types/main_types';
+import { PropType, defineComponent } from 'vue';
+import PosDialog from './PosDialog.vue';
 
 export default defineComponent({
+  name: 'XposDialog',
+  components: {
+    PosDialog
+  },
   props: {
     sentenceBus: {
       type: Object as PropType<sentence_bus_t>,
       required: true,
     },
   },
-  data() {
-    const token = emptyTokenJson();
-    return {
-      xposDialogOpened: false,
-      token,
-      userId: '',
-    };
-  },
   computed: {
     ...mapState(useProjectStore, ['annotationFeatures']),
-  },
-  mounted() {
-    this.sentenceBus.on('open:xposDialog', ({ token, userId }) => {
-      this.token = token;
-      this.userId = userId;
-      this.xposDialogOpened = true;
-    });
-  },
-  beforeUnmount() {
-    this.sentenceBus.off('open:xposDialog');
-  },
-  methods: {
-    onChangeXpos() {
-      this.xposDialogOpened = false;
-      this.sentenceBus.emit('tree-update:token', {
-        token: this.token,
-        userId: this.userId,
-      });
-    },
-    onDeleteXpos() {
-      this.token.XPOS = '_';
-      this.sentenceBus.emit('tree-update:token', {
-        token: this.token,
-        userId: this.userId,
-      });
-    },
   },
 });
 </script>

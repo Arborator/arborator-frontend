8762294f1a62a061c90a86966ef4af4f36700cf6
khansadaoudi <hk_daoudi@esi.dz>
2024-02-08 16:59:54 +0100

    change conll dialog to conll table

--- src/components/sentence/ConlluDialog.vue
+++ src/components/sentence/ConlluDialog.vue
@@ -1,74 +1,52 @@
 <template>
   <q-dialog v-model="conlluDialogOpened" full-width>
-    <q-layout view="lHr LpR lFf" container style="background-color: #282a36">
-      <q-header elevated class="bg-primary text-white">
-        <q-toolbar>
-          <q-toolbar-title>{{ $t('sentenceCard.conllText') }}</q-toolbar-title>
-          <q-btn v-close-popup flat round dense icon="close" />
-        </q-toolbar>
-      </q-header>
-      <q-page-container>
-        <Codemirror v-model:value="conllContent" :options="cmOption" class="CodeMirror" @focus="codefocus"> </Codemirror>
-      </q-page-container>
-      <q-footer>
-        <q-toolbar inset>
+    <q-card>
+        <q-bar class="bg-primary text-white sticky-bar">
+          <div class="text-weight-bold">Conll Dialog</div>
           <q-space />
-          <q-btn v-close-popup flat no-caps :label="$t('cancel')" />
-          <q-btn v-close-popup color="primary" label="Ok" :disabled="currentConllContent === conllContent" @click="onConllDialogOk()" />
-        </q-toolbar>
-      </q-footer>
-    </q-layout>
+          <q-btn v-close-popup flat dense icon="close" />
+        </q-bar>
+        <q-card-section class="q-pa-none">
+          <q-table
+            flat
+            :rows="conllTable"
+            :columns="table.fields"
+            class="my-sticky-header-table"
+            hide-bottom
+            :rows-per-page-options="[0]"
+            separator="cell"
+          >
+            <template v-slot:body="props">
+              <q-tr :props="props">
+                <q-td v-for="col of conllColumns" :key="col" :props="props">
+                  {{ props.row[col] }}
+                    <q-popup-edit v-if="col !== 'ID'" v-model="props.row[col]" auto-save v-slot="scope" >
+                      <q-input  
+                        dense
+                        autofocus
+                        v-model="scope.value" 
+                       />
+                    </q-popup-edit>
+                </q-td>
+              </q-tr>
+            </template>
+          </q-table>
+        </q-card-section>
+        <q-card-actions class="sticky-card-actions" align="around">
+          <q-btn v-close-popup outline color="primary" :label="$t('cancel')" style="width: 45%; margin-left: auto; margin-right: auto" />
+          <q-btn v-close-popup color="primary" label="Ok" style="width: 45%; margin-left: auto; margin-right: auto" @click="onConllDialogOk()" />
+        </q-card-actions>
+    </q-card>
   </q-dialog>
 </template>
 
 <script lang="ts">
-import { sentenceConllToJson } from 'conllup/lib/conll';
-import CodeMirror2 from 'codemirror';
-import Codemirror from 'codemirror-editor-vue3';
-import 'codemirror-editor-vue3/dist/style.css';
-import 'codemirror/mode/javascript/javascript.js';
-import 'codemirror/theme/dracula.css';
-
-import { sentence_bus_t } from 'src/types/main_types';
-import { notifyError, notifyMessage } from 'src/utils/notify';
+import { tokenJson_T, _featuresConllToJson, _depsConllToJson, nodesJson_T } from 'conllup/lib/conll';
+import { sentence_bus_t, table_t } from 'src/types/main_types';
+import { notifyMessage } from 'src/utils/notify';
 import { PropType, defineComponent } from 'vue';
 
-CodeMirror2.defineMode('tsv', () => {
-  function tokenBase(stream: any) {
-    if (stream.string.match(/^#.+/)) {
-      stream.skipToEnd();
-      return 'comment';
-    }
-
-    const ch = stream.next();
-
-    if (/\d/.test(ch)) {
-      stream.eatWhile(/[\d]/);
-      if (stream.eat('.')) {
-        stream.eatWhile(/[\d]/);
-      }
-      return 'number';
-    }
-    if (/[+\-*&%=<>!?|:]/.test(ch)) {
-      return 'operator';
-    }
-    stream.eatWhile(/\w/);
-    return 'variable';
-  }
-
-  return {
-    startState() {
-      return { tokenize: tokenBase, commentLevel: 0 };
-    },
-    token(stream: any, state: any) {
-      if (stream.eatSpace()) return null;
-      return state.tokenize(stream, state);
-    },
-    lineComment: '#',
-  };
-});
 export default defineComponent({
-  components: { Codemirror },
   props: {
     sentenceBus: {
       type: Object as PropType<sentence_bus_t>,
@@ -76,69 +54,100 @@ export default defineComponent({
     },
   },
   data() {
+    const conllTable: any[] = [];
+    const conllColumns = ['ID', 'FORM', 'LEMMA', 'UPOS', 'XPOS', 'FEATS', 'HEAD', 'DEPREL', 'DEPS', 'MISC'];
+    const nodesJson: nodesJson_T = {};
+    const table: table_t<tokenJson_T> = {
+      fields: [],
+      visibleColumns: [],
+      filter: '',
+      selected: [],
+      loading: false,
+      pagination: {
+        sortBy: 'key',
+        descending: false,
+        page: 1,
+        rowsPerPage: 10,
+      },
+      loadingDelete: false,
+      exporting: false,
+    };
     return {
+      conllTable,
+      conllColumns,
+      nodesJson,
+      table,
       userId: '',
       conlluDialogOpened: false,
-      currentConllContent: '',
-      conllContent: '',
-      cmOption: {
-        tabSize: 8,
-        styleActiveLine: true,
-        lineNumbers: false,
-        lineWrapping: true,
-        line: true,
-        mode: 'tsv',
-        theme: 'dracula',
-      },
     };
   },
   mounted() {
     this.sentenceBus.on('open:conlluDialog', ({ userId }) => {
       this.userId = userId;
       this.conlluDialogOpened = true;
-      this.currentConllContent = this.sentenceBus.sentenceSVGs[this.userId].exportConll();
-      this.conllContent = this.sentenceBus.sentenceSVGs[this.userId].exportConll();
+      this.nodesJson = this.sentenceBus.sentenceSVGs[this.userId].treeJson.nodesJson;
+      console.log(this.nodesJson)
+      this.getConllTable();
     });
+    this.createTableFields();
   },
   methods: {
-    codefocus(cm: any, ev: any) {
-      cm.refresh();
-      cm.execCommand('selectAll');
-      console.log('codefocus()', ev);
+    getConllTable() {
+      this.conllTable = [];
+      for (const node of Object.values(this.nodesJson)) {
+        this.conllTable.push({
+          ...node,
+          FEATS: this.formatTableEntry(node.FEATS),
+          DEPS: this.formatTableEntry(node.DEPS),
+          MISC: this.formatTableEntry(node.MISC),
+        });
+      }
+    },
+    formatTableEntry(entry: any) {
+      return Object.entries(entry)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('|');
+    },
+    createTableFields() {
+      this.conllColumns.forEach((column) => {
+        this.table.fields.push({
+          name: column,
+          label: column,
+          sortable: false,
+          align: 'left',
+          field: column,
+        });
+      });
     },
     onConllDialogOk() {
-      let sentenceJson;
-      sentenceJson = sentenceConllToJson(this.conllContent);
-      if (!this.isMetaChanged()) {
-        this.sentenceBus.emit('tree-update:sentence', {
-          sentenceJson,
-          userId: this.userId,
-        });
-        notifyMessage({
+      this.conllTable.forEach((row, index) => {
+        console.log(_featuresConllToJson(row.FEATS))
+        this.nodesJson[`${index + 1}`] = {
+          ...row, 
+          FEATS: row.FEATS ? _featuresConllToJson(row.FEATS) : {},
+          MISC: row.MISC ? _featuresConllToJson(row.MISC) : {},
+          DEPS: row.DEPS ? _depsConllToJson(row.DEPS) : {},
+        }
+      }); 
+      this.sentenceBus.sentenceSVGs[this.userId].treeJson.nodesJson = { ...this.nodesJson };
+      this.sentenceBus.emit('tree-update:sentence', {
+        sentenceJson: {
+          metaJson: this.sentenceBus.sentenceSVGs[this.userId].metaJson,
+          treeJson: this.sentenceBus.sentenceSVGs[this.userId].treeJson,
+        },
+        userId: this.userId,
+      });  
+      notifyMessage({
           message: "Conllu changed locally, don't forget to save !",
           type: 'warning',
           icon: 'warning',
-        });
-      } else {
-        notifyError({
-          error: 'Changing timestamp, user_id, sent_id is not allowed !',
-        });
-      }
-    },
-    isMetaChanged() {
-      const fixedMetadata = ['timestamp', 'user_id', 'sent_id'];
-      const oldMeta = this.sentenceBus.sentenceSVGs[this.userId].metaJson;
-      const newMeta = sentenceConllToJson(this.conllContent).metaJson;
-      let isMetaChanged = false;
-      for (const metaKey of fixedMetadata) {
-        if (!Object.keys(newMeta).includes(metaKey) || oldMeta[metaKey] !== newMeta[metaKey]) {
-          isMetaChanged = true;
-        }
-      }
-      return isMetaChanged;
+      });   
     },
   },
 });
 </script>
 
-<style></style>
+<style>
+
+
+</style>

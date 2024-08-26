c476fed6e6d15f92300337be759df1c3a5ffb3e2
khansadaoudi <hk_daoudi@esi.dz>
2024-08-26 09:52:55 +0200

    Fix the problem of old access

--- src/utils/timeAgoUtils.ts
+++ src/utils/timeAgoUtils.ts
@@ -2,8 +2,6 @@ import { i18n } from 'src/boot/i18n';
 
 export function timeAgo(secsAgo: number) {
   const formatter = new Intl.RelativeTimeFormat(i18n.global.locale.value, { style: 'long' });
-  // const ranges: { [key in Intl.RelativeTimeFormatUnit]?: number } = {
-  // const ranges: Partial<Record<Intl.RelativeTimeFormatUnit, number>> = {
   const ranges: { [key: string]: number } = {
     years: 3600 * 24 * 365,
     months: 3600 * 24 * 30,
@@ -16,8 +14,13 @@ export function timeAgo(secsAgo: number) {
   for (const key in ranges) {
     if (ranges[key] < Math.abs(secsAgo)) {
       const delta = secsAgo / ranges[key];
-      return formatter.format(Math.round(delta), key as Intl.RelativeTimeFormatUnit);
-    }
+      if (Math.round(delta) < -4 && key === 'years') {
+        return i18n.global.locale.value === 'en' ? 'a long time ago' : 'Il y a longtemps'
+      }
+      else {
+        return formatter.format(Math.round(delta), key as Intl.RelativeTimeFormatUnit);
+      }
+    }    
   }
   return formatter.format(secsAgo, 'seconds'); // should be useless
 }

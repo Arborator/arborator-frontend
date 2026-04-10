04a03372f14db69cbf059f88e9a74b3cb91e4944
RomainSabat <rsabatier57@gmail.com>
2026-04-10 14:46:40 +0200

    improve video appearance

--- src/components/sentence/Video.vue
+++ src/components/sentence/Video.vue
@@ -3,14 +3,33 @@
     v-show="!isVideoHidden"
     ref="videoDiv"
     class="fixed z-max videoSize"
-    :style="{ top: y + 'px', left: x + 'px' }"
+    :style="{ top: y + 'px', left: x + 'px', width: videoWidth + '%' }"
   >
-
     <q-card
     @mousedown="startDrag"
     @mouseup="stopDrag"
     >
-      <q-bar class="drag-handle bg-primary text-white " >
+      <q-bar class="drag-handle bg-primary text-white" >
+       <q-btn
+       icon="speed"
+       flat
+       @click="showSpeedBtn = !showSpeedBtn"
+       />
+        <q-btn-toggle
+          v-show="showSpeedBtn"
+          v-model="videoSpeed"
+          flat
+          no-caps
+          toggle-color="grey"
+          :options="[
+            {label: 'x0.25', value: '0.25'},
+            {label: 'x0.5', value: '0.5'},
+            {label: 'x1', value: '1'},
+            {label: 'x1.5', value: '1.5'},
+          ]"
+          @update:model-value="changeVideoSpeed()"
+        />
+
         <q-space />
         <q-btn dense flat icon="close" @click="toggleVideo()"/>
       </q-bar>
@@ -22,23 +41,17 @@
         preload="auto"
         class="video"
         @play="changeVideoSpeed()"
-        :playbackRate="Number(model)"
+        :playbackRate="Number(videoSpeed)"
         disablePictureInPicture
       >
       </video>
 
-      <div class="speedChoice q-pl-md q-pr-md" >
-        <q-select
-          color="primary : purple-7"
-          v-model="model"
-          :options="options"
-          @update:model-value="changeVideoSpeed()"
-        >
-          <template v-slot:prepend>
-            <q-icon name="speed"/>
-          </template>
-        </q-select>
-      </div>
+      <q-badge
+        class="resize-handle "
+        @mousedown="startResize"
+      >
+        <q-icon name="crop" color="transparent"/>
+      </q-badge>
     </q-card>
   </div>
 
@@ -51,13 +64,12 @@
     >
       {{ hideText }}
     </q-btn>
-
   </div>
 
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 import { mapState, mapWritableState } from 'pinia';
 import { useTreesStore } from 'src/pinia/modules/trees';
@@ -84,9 +96,12 @@ export default defineComponent({
       dragging: false,
       offsetX: 0,
       offsetY: 0,
-      model: '1',
       options: ['0.25', '0.5', '1', '1.5', '2'],
       cachedVideoUrl: '',
+      videoSpeed: '1',
+      showSpeedBtn: true,
+      videoWidth: 25,
+      isResizing: false,
     }
   },
   computed: {
@@ -117,12 +132,13 @@ export default defineComponent({
       return this.videoUrl.toString()
     },
     moveDiv(e : MouseEvent) {
-      if (!this.dragging) return
+      if (!this.dragging ) return
       //set new position to mouse position
       this.x = e.clientX - this.offsetX
       this.y = e.clientY - this.offsetY
     },
     startDrag(e: MouseEvent) {
+      if(this.isResizing) return
       this.dragging = true
       //calcul distance between div and mouse
       this.offsetX = e.clientX - this.x
@@ -135,34 +151,66 @@ export default defineComponent({
     },
     changeVideoSpeed(){
       if (this.videoRef){
-        this.videoRef.playbackRate = Number(this.model)
+        this.videoRef.playbackRate = Number(this.videoSpeed)
       }
     },
     setVideoPosition(){
       //set video position to 70% of window's width
       this.x = window.innerWidth * 0.7
     },
+    startResize(e: MouseEvent) {
+      this.isResizing = true;
+      // Store the mouse position to calculate resizing
+      this.offsetX = e.clientX;
+      this.offsetY = e.clientY;
+      document.addEventListener('mousemove', this.resize);
+      document.addEventListener('mouseup', this.stopResize);
+    },
+    resize(e: MouseEvent) {
+      if (!this.isResizing) return;
+      // Calculate different of mouse position
+      const widthChange = this.offsetX - e.clientX - 0.5
+      // Update width based on mouse movement
+      this.videoWidth = Math.max(10, this.videoWidth + (widthChange / window.innerWidth) * 100);
+
+      //move video to the left
+      this.x = this.x - widthChange;
+
+      // Update the mouse offset for next movement
+      this.offsetX = e.clientX;
+      this.offsetY = e.clientY;
+    },
+    stopResize() {
+      this.isResizing = false;
+      document.removeEventListener('mousemove', this.resize);
+      document.removeEventListener('mouseup', this.stopResize);
+    },
   }
 })
 </script>
-<style lang="css">
-.videoSize{
-  width: 25%;
-  height:auto;
-  text-align: center;
-  resize: horizontal;
-  overflow: auto;
-  min-width: 100px;
-}
-.video{
-  width: 100%;
-  height: auto;
-}
-.backgroundBtn{
-  background-color: "gray";
-}
-.speedChoice{
-  height: auto;
-  width: 22%;
-}
+  <style lang="css">
+  .videoSize{
+    width: 25%;
+    height:auto;
+    text-align: center;
+    resize: horizontal;
+    overflow: auto;
+    min-width: 100px;
+  }
+  .video{
+    width: 100%;
+    height: auto;
+  }
+  .resize-handle {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 10px;
+    height: 10px;
+    cursor: ew-resize;
+    background-color: transparent;
+  }
+  .bold{
+    font-weight: bold;
+  }
 </style>
